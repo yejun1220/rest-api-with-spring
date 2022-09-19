a513e157bb050ac716d0d3642f6b100696844dd0
im-yejun <yejun_1220@naver.com>
2022-09-19 10:50:26 +0900

    event domain

---
+++ src/main/java/events/Event.java
@@ -0,0 +1,27 @@
+package events;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Builder @AllArgsConstructor @NoArgsConstructor
+@Getter @Setter @EqualsAndHashCode(of = "id")
+public class Event {
+
+    private Integer id;
+    private String name;
+    private String description;
+    private LocalDateTime beginEnrollmentDateTime;
+    private LocalDateTime closeEnrollmentDateTime;
+    private LocalDateTime beginEventDateTime;
+    private LocalDateTime endEventDateTime;
+    private String location;
+    private int basePrice;
+    private int maxPrice;
+    private int limitOfEnrollment;
+    private boolean offline;
+    private boolean free;
+    private EventStatus eventStatus;
+
+
+}
---
+++ src/test/java/events/EventTest.java
@@ -0,0 +1,30 @@
+package events;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class EventTest {
+
+    @Test
+    void builder() {
+        Event event = Event.builder()
+                .name("")
+                .build();
+
+        assertThat(event).isNotNull();
+    }
+
+    @Test
+    void javaBeanSpec() {
+        // given
+        String name = "Event";
+
+        // when
+        Event event = new Event();
+        event.setName(name);
+
+        // then
+        assertThat(event.getName()).isEqualTo(name);
+    }
+}
