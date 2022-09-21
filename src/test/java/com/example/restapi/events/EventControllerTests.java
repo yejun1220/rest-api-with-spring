ea3f8df3041f768ab5bfc03e66a4ffbdcc0a90b8
im-yejun <yejun_1220@naver.com>
2022-09-21 16:35:02 +0900

    fail on unknown properites

--- src/test/java/com/example/restapi/events/EventControllerTests.java
+++ src/test/java/com/example/restapi/events/EventControllerTests.java
@@ -3,7 +3,6 @@ package com.example.restapi.events;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -14,7 +13,6 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.time.LocalDateTime;
-import java.util.regex.Matcher;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -35,15 +33,12 @@ public class EventControllerTests {
 
     @Test
     void createEvent() throws Exception {
-        Event event = Event.builder()
-                .id(100)
+        EventDto event = EventDto.builder()
                 .name("Spring")
                 .description("description")
                 .beginEnrollmentDateTime(LocalDateTime.of(2022, 1, 1, 12,0))
                 .closeEnrollmentDateTime(LocalDateTime.of(2022, 1, 2, 12,0))
                 .location("location")
-                .free(true)
-                .eventStatus(EventStatus.PUBLISHED)
                 .build();
 
         // perform 안에 요청을 준다. 응답이 나온다.
@@ -60,6 +55,29 @@ public class EventControllerTests {
                 .andExpect(jsonPath("id").value(Matchers.not(100)))
                 .andExpect(jsonPath("free").value(Matchers.not(true)))
                 .andExpect(jsonPath("eventStatus").value(EventStatus.DRAFT.name()))
+        ;
+    }
+
+    @Test
+    void createEvent_Bad_Request() throws Exception {
+        Event event = Event.builder()
+                .id(100)
+                .name("Spring")
+                .description("description")
+                .beginEnrollmentDateTime(LocalDateTime.of(2022, 1, 1, 12,0))
+                .closeEnrollmentDateTime(LocalDateTime.of(2022, 1, 2, 12,0))
+                .location("location")
+                .free(true)
+                .eventStatus(EventStatus.PUBLISHED)
+                .build();
+
+        mockMvc.perform(post("/api/events/")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaTypes.HAL_JSON)
+                        .content(objectMapper.writeValueAsString(event))
+                )
+                .andDo(print())
+                .andExpect(status().isBadRequest())
         ;
     }
 }
