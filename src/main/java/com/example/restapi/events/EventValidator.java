f32566cc46fb8a57890ac727dc068390b41d8774
im-yejun <yejun_1220@naver.com>
2022-09-27 21:47:36 +0900

    validate wrong data

--- src/main/java/com/example/restapi/events/EventValidator.java
+++ src/main/java/com/example/restapi/events/EventValidator.java
@@ -3,11 +3,21 @@ package com.example.restapi.events;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 
+import java.time.LocalDateTime;
+
 @Component
 public class EventValidator {
     public void validEventDto(EventDto eventDto, Errors errors) {
-        if(eventDto.getBasePrice() > eventDto.getMaxPrice() && eventDto.getMaxPrice() > 0) {
-        errors.rejectValue("basePrice", "wrongValue", "basePrice is wrong");
+        if (eventDto.getBasePrice() > eventDto.getMaxPrice() && eventDto.getMaxPrice() > 0) {
+            errors.rejectValue("basePrice", "wrongValue", "basePrice is wrong");
+            errors.rejectValue("maxPrice", "wrongValue", "maxPrice is wrong");
+        }
+
+        LocalDateTime endEventDateTime = eventDto.getEndEventDateTime();
+        if (endEventDateTime.isBefore(eventDto.getBeginEventDateTime()) ||
+                endEventDateTime.isBefore(eventDto.getCloseEnrollmentDateTime()) ||
+                endEventDateTime.isBefore((eventDto.getBeginEnrollmentDateTime()))) {
+            errors.rejectValue("endEventDateTime", "wrongValue", "endEventDateTime is wrong");
         }
     }
 }
