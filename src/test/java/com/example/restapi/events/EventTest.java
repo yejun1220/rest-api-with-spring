3f187aab8121826d05dd7fb3bfd456bd48bc6572
im-yejun <yejun_1220@naver.com>
2022-09-28 23:03:35 +0900

    Business logic tests

--- src/test/java/com/example/restapi/events/EventTest.java
+++ src/test/java/com/example/restapi/events/EventTest.java
@@ -1,6 +1,8 @@
 package com.example.restapi.events;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -26,5 +28,45 @@ class EventTest {
 
         // then
         assertThat(event.getName()).isEqualTo(name);
+    }
+
+    @ParameterizedTest(name = "{index}: basePrice={0}, maxPrice={1}, isFree={2}")
+    @CsvSource({
+            "0, 0, true",
+            "100, 0, false",
+            "0, 1000, false"
+    })
+    void testFree(int basePrice, int maxPrice, boolean isFree) {
+        // given
+        Event freeEvent = Event.builder()
+                .basePrice(basePrice)
+                .maxPrice(maxPrice)
+                .build();
+
+        // when
+        freeEvent.update();
+
+        // then
+        assertThat(freeEvent.isFree()).isEqualTo(isFree);
+
+    }
+
+    @ParameterizedTest(name = "{index}: location={0}, isOffline={1}")
+    @CsvSource({
+            "강남, true",
+            ", false"
+
+    })
+    void testOffline(String location, boolean isOffline) {
+        // given
+        Event hasLocationEvent = Event.builder()
+                .location(location)
+                .build();
+
+        // when
+        hasLocationEvent.update();
+
+        // then
+        assertThat(hasLocationEvent.isOffline()).isEqualTo(isOffline);
     }
 }
